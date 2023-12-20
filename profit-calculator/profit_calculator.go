a027b20f105061f1d998c36ae6357c5e517621cf
pakzad <pakzadlive@gmail.com>
2023-12-20 12:13:58 +0100

    Exercise: Working with functions

--- profit-calculator/profit_calculator.go
+++ profit-calculator/profit_calculator.go
@@ -3,24 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	// var revenue float64
+	// var expenses float64
+	// var taxRate float64
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+	// fmt.Print()
+	// fmt.Scan(&expenses)
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
+	// fmt.Print("Tax Rate: ")
+	// fmt.Scan(&taxRate)
 
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
+	revenue := getUserInput("Revenue: ")
+	expenses := getUserInput("Expenses: ")
+	taxRate := getUserInput("Tax Rate: ")
 
+	ebt, profit, ratio := calclateFacals(revenue, expenses, taxRate)
+
+	fmt.Printf("%.1f\n", ebt)
+	fmt.Printf("%.1f\n", profit)
+	fmt.Printf("%.1f\n",ratio)
+}
+
+func calclateFacals(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
 
-	fmt.Println(ebt)
-	fmt.Println(profit)
-	fmt.Println(ratio)
+	return ebt, profit, ratio
+}
+
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
 }

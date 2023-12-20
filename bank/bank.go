d16c7793bda5e67d7e64085f0761665ad1890b02
pakzad <pakzadlive@gmail.com>
2023-12-20 13:13:45 +0100

    loops

--- bank/bank.go
+++ bank/bank.go
@@ -4,52 +4,61 @@ import "fmt"
 
 func main() {
 	var accountBalance float64 = 1000
-
 	fmt.Println("Welcome to Go Bank!")
-	fmt.Println("Wat do you want to do?")
-	fmt.Println("1. Check balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
 
-	var choice int
-	fmt.Print("your choice:")
-	fmt.Scan(&choice)
+	// for i := 0; i < 200; i++ {
+	for { // Infinite loop
 
-	// wantsCheckBalance := choice == 1
+		fmt.Println("Wat do you want to do?")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
 
-	if choice == 1 {
-		fmt.Println("Your balance is:", accountBalance)
-	} else if choice == 2 {
-		var depositAmount float64
-		fmt.Print("Your deposit: ")
-		fmt.Scan(&depositAmount)
+		var choice int
+		fmt.Print("your choice:")
+		fmt.Scan(&choice)
 
-		if depositAmount <= 0 {
-			fmt.Println("Invalid amount. Must be greater than 0.")
-			return
+		// wantsCheckBalance := choice == 1
+
+		if choice == 1 {
+			fmt.Println("Your balance is:", accountBalance)
+		} else if choice == 2 {
+			var depositAmount float64
+			fmt.Print("Your deposit: ")
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				// return
+				continue
+			}
+
+			accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
+
+			fmt.Println("Balance updated! New amount:", accountBalance)
+		} else if choice == 3 {
+			var withdrawAmount float64
+			fmt.Print("Withdraw amount: ")
+			fmt.Scan(&withdrawAmount)
+
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+			if withdrawAmount > accountBalance {
+				fmt.Println("Invalid amount. You can't withdraw more than you have.")
+				continue
+			}
+
+			accountBalance -= withdrawAmount
+			fmt.Println("Balance updated! New amount:", accountBalance)
+		} else {
+			fmt.Println("Goodbye!")
+			// return
+			break
 		}
-
-		accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
-
-		fmt.Println("Balance updated! New amount:", accountBalance)
-	} else if choice == 3 {
-		var withdrawAmount float64
-		fmt.Print("Withdraw amount: ")
-		fmt.Scan(&withdrawAmount)
-
-		if withdrawAmount <= 0 {
-			fmt.Println("Invalid amount. Must be greater than 0.")
-			return
-		}
-		if withdrawAmount > accountBalance {
-			fmt.Println("Invalid amount. You can't withdraw more than you have.")
-			return
-		}
-
-		accountBalance -= withdrawAmount
-		fmt.Println("Balance updated! New amount:", accountBalance)
-	} else {
-		fmt.Println("Goodbye!")
 	}
+
+	fmt.Println("Thanks for choosing our Bank")
 }

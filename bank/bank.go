f45bc4dd5268967758e85757b83c9296477411c2
pakzad <pakzadlive@gmail.com>
2023-12-25 15:54:50 +0100

    Third-Party packages

--- bank/bank.go
+++ bank/bank.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"example.com/bank/fileops"
+	"github.com/Pallinder/go-randomdata"
 )
 
 const accountBalanceFile = "balance.txt"
@@ -19,6 +20,7 @@ func main() {
 	}
 
 	fmt.Println("Welcome to Go Bank!")
+	fmt.Println("Reach us 24/7", randomdata.PhoneNumber())
 
 	// for i := 0; i < 200; i++ {
 	for { // Infinite loop

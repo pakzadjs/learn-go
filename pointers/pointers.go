cccb33019752b33f0ace512d70f3c6473e0e8765
pakzad <pakzadlive@gmail.com>
2023-12-26 13:51:54 +0100

    Creating a Pointer

---
+++ pointers/pointers.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 32 //regular variable
+
+	// var agePointer *int
+	// agePointer = &age
+
+	agePointer := &age
+
+	fmt.Println("Age:", *agePointer)
+
+	adultYears := getAdultYears(&age)
+	fmt.Println(adultYears)
+}
+
+func getAdultYears(age *int) int {
+	return *age - 18
+}

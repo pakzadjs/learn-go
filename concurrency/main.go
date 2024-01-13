ce62d808a04fb67f7ee8063ad968a54ef7badc74
pakzad <pakzadlive@gmail.com>
2024-01-13 22:03:47 +0100

    Multiple channels and Goroutines

--- concurrency/main.go
+++ concurrency/main.go
@@ -5,21 +5,36 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneCHan chan bool) {
 	fmt.Println("Hello!", phrase)
+	doneCHan <- true
 }
 
 func slowGreet(phrase string, doneCHan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
 	doneCHan <- true
+	close(doneCHan)
 }
 
 func main() {
-	// go greet("Nice to meet you!")
-	// go greet("How are you?")
+	// dones := make([]chan bool, 4)
 	done := make(chan bool)
+
+	// dones[0] = make(chan bool)
+	go greet("Nice to meet you!", done)
+	// dones[1] = make(chan bool)
+	go greet("How are you?", done)
+	// dones[2] = make(chan bool)
 	go slowGreet("How ... are ... you ...?", done)
-	// go greet("I hope you're liking the course!")
-	<- done
+	// dones[3] = make(chan bool)
+	go greet("I hope you're liking the course!", done)
+
+	// for _, done := range dones {
+	// 	<-done
+	// }
+
+	for range done {
+		// fmt.Println(doneChan)
+	}
 }

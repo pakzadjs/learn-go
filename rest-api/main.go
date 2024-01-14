5369509b377e5f106b1f4a48db2d0959ca4e1196
pakzad <pakzadlive@gmail.com>
2024-01-14 15:29:36 +0100

    Creating A SQL database Table

--- rest-api/main.go
+++ rest-api/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"example.com/rest-api/db"
 	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)

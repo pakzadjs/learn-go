8d67b0ec365a6b98b1216a1c10fcd5478a317b12
pakzad <pakzadlive@gmail.com>
2024-01-15 17:21:40 +0100

    Finishing the JWT logic

--- rest-api/routes/users.go
+++ rest-api/routes/users.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"example.com/rest-api/models"
+	"example.com/rest-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -44,5 +45,12 @@ func login(context *gin.Context) {
 		return
 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "Login successfully!."})
+	token, err := utils.GenerateToken(user.Email, user.ID)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not authenticate user", "error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successfully!", "token": token})
 }

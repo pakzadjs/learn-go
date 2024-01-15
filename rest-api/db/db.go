0554436fecdd86b004f50235d7bf553235bb309c
pakzad <pakzadlive@gmail.com>
2024-01-15 14:20:21 +0100

    Adding users table

--- rest-api/db/db.go
+++ rest-api/db/db.go
@@ -22,6 +22,20 @@ func InitDB() {
 }
 
 func createTables() {
+	createUsersTable :=`
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
+	)
+	`
+
+	_, err := DB.Exec(createUsersTable)
+
+	if err != nil {
+		panic("Could not create users table: " + err.Error())
+	}
+
 	createEventsTable := `
 	CREATE TABLE IF NOT EXISTS events (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -30,10 +44,11 @@ func createTables() {
 			location TEXT NOT NULL,
 			dateTime DATETIME NOT NULL,
 			user_id INTEGER
+			FOREIGN KEY(user_id) REFERENCES users(id)
 	)
 `
 
-	_, err := DB.Exec(createEventsTable)
+	_, err = DB.Exec(createEventsTable)
 
 	if err != nil {
 		panic("Could not create events table: " + err.Error())
